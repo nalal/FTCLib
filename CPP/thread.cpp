71ff628e282c3ca7d20a62192c4889784862e55c
nalal <noahgman11@gmail.com>
2019-09-03 13:20:40 -0600

    added comments

--- CPP/thread.cpp
+++ CPP/thread.cpp
@@ -3,11 +3,13 @@
 
 using std::thread;
 using std::this_thread;
+using std::string;
 
 const int threads = hardware_concurrency();;
 int usedThreads = 0;
-threadInfo threadList[threads];
+threadInfo threadList[(threads - 1)];
 
+//Incomplete
 void stripeThreadRaw(void (*func))
 {
 	for(uint8_t i = 0; i < threads; i++)
@@ -20,11 +22,23 @@ void stripeThreadRaw(void (*func))
 	}
 }
 
+//Incomplete
+void setThreads()
+{
+	for(uint8_t i = 0; i < threads; i++)
+	{
+		threadInfo threadDetail;
+		threadList[i] = threadDetail;
+	}
+}
+
+//Get total threads
 int getThreads()
 {
 	return threads();
 }
 
+//Get total used threads
 int getThreadsUsed()
 {
 	return usedThreads;
--- CPP/thread.h
+++ CPP/thread.h
@@ -3,6 +3,7 @@
 // C includes
 #include <thread.h>
 #include <stdint.h>
+#include <string.h>
 
 // Local includes
 
